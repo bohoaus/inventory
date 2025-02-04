b37fffebd6d18ddd7b30536d1b9826f8b8368e87
bohoaus <bohoaus2012@gmail.com>
2025-02-05 00:16:24 +1100

    Update odmItem.js

--- components/dedicated/odmItem.js
+++ components/dedicated/odmItem.js
@@ -252,7 +252,7 @@ class OdmItem {
             </div>
 
             <div class="form-group required">
-                <label for="pack_size">Pack Size{"S+M+L+XL": 1}</label>
+                <label for="pack_size">Pack Size*{"S+M+L+XL": 1}</label>
                 <input type="text" 
                        name="pack_size" placeholder="{"S+M+L+XL": 1}"
                        value="${item?.pack_size || ""}"
@@ -260,7 +260,7 @@ class OdmItem {
             </div>
 
              <div class="form-group required">
-                <label for="pack_unit">Pack Unit</label>
+                <label for="pack_unit">Pack Unit*</label>
                 <input type="number" 
                        name="pack_unit" value="1" 
                        step="1" 
