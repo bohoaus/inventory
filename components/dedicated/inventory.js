b5eef6f0a877edd1dff56e0f8207c23f359d5833
bohoaus <bohoaus2012@gmail.com>
2025-02-15 09:44:23 +1100

    Update inventory.js

--- components/dedicated/inventory.js
+++ components/dedicated/inventory.js
@@ -171,7 +171,7 @@ class InventoryComponent {
           </button>
           <select id="rowsPerPage">
             <option value="10">10 per page</option>
-            <option value="25">25 per page</option>
+            <option value="25" selected>25 per page</option>
             <option value="50">50 per page</option>
             <option value="100">100 per page</option>
           </select>
