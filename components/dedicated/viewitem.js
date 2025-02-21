36721b7ea8bc5c13f69cbcf1906fa74cd972793e
bohoaus <bohoaus2012@gmail.com>
2025-02-21 22:21:27 +1100

    Update viewitem.js

--- components/dedicated/viewitem.js
+++ components/dedicated/viewitem.js
@@ -225,11 +225,11 @@ class ViewItem {
                 <h3>Sales Info:</h3>
                 <div class="detail-row">
                     <span class="label">WSP:</span>
-                    <span class="value">${item.swsp.toFixed(2)  || "-"}</span>
+                    <span class="value">${item.swsp  || "-"}</span>
                 </div>
                 <div class="detail-row">
                     <span class="label">WSP2:</span>
-                    <span class="value">${item.swsp2.toFixed(2) || "-"}</span>
+                    <span class="value">${item.swsp2 || "-"}</span>
                 </div>
             </div>
         </div>
