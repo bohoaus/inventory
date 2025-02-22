a55bef045fbb755a73a3623df4a476529c87a39e
bohoaus <bohoaus2012@gmail.com>
2025-02-23 00:29:22 +1100

    Update viewOrder.js

--- components/dedicated/viewOrder.js
+++ components/dedicated/viewOrder.js
@@ -248,13 +248,17 @@ class ViewOrder {
                 ? `
                 <div class="detail-item">
                     <label>Tracking#:</label>
-                    <span>${orderData.tracking_no || "-"}</span>
+                    <span style="color:blue">${orderData.tracking_no || "-"}</span>
                 </div>
                 <div class="detail-item">
                 </div>
             <div class="detail-item">
+                <label>PPO#:</label>
+                <span style="color:blue">${orderData.opo || "-"}</span>
+            </div>
+            <div class="detail-item">
                 <label>Order Note:</label>
-                <span>${orderData.order_note || "-"}</span>
+                <span style="color:blue">${orderData.order_note || "-"}</span>
             </div>
             `
                 : ""
