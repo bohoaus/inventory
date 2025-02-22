a631fdedc46843de59d4e8624e26e9a3e2297a52
bohoaus <bohoaus2012@gmail.com>
2025-02-22 11:16:38 +1100

    Update editWholesaleOrder.js

--- components/dedicated/editWholesaleOrder.js
+++ components/dedicated/editWholesaleOrder.js
@@ -107,7 +107,7 @@ class EditWholesaleOrder {
           <div class="editwholesaleorder-section">
             <div class="editwholesaleorder-row">
               <div class="editwholesaleorder-group" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
-                        <label style="text-align:left; display: block; width: 200px">Customer Name</label>
+                        <label style="text-align:left; display: block; width: 300px">Customer Name</label>
                         <input type="text" value="${
                           orderData.customer_name || ""
                         }" disabled>
