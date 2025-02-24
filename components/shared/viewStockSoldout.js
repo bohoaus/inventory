ee560fb797f7e1bc3ac55b1886d10ecb8c8d55fd
bohoaus <bohoaus2012@gmail.com>
2025-02-24 15:27:44 +1100

    Update viewStockSoldout.js

--- components/shared/viewStockSoldout.js
+++ components/shared/viewStockSoldout.js
@@ -62,9 +62,9 @@ class StaffSoldout {
                   <th>Code</th>
                   <th>Colour</th>
                   <th>Name</th>
-                  <th>Release Date</th>
-                  <th>Sold Out Date</th>
-                  <th>Selling Time</th>
+                  <th>ReleaseDate</th>
+                  <th>SoldOutDate</th>
+                  <th>SellingTime</th>
                   <th>Status</th>
                 </tr>
               </thead>
