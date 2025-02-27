b59299e4f15fea6ce3223f1e17601fa873c981a9
bohoaus <bohoaus2012@gmail.com>
2025-02-27 21:34:21 +1100

    Update editWholesaleOrder.js

--- components/dedicated/editWholesaleOrder.js
+++ components/dedicated/editWholesaleOrder.js
@@ -114,7 +114,7 @@ class EditWholesaleOrder {
             <div class="editwholesaleorder-row">
               <div class="editwholesaleorder-group" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
                         <label style="text-align:left; display: block; width: 200px">Customer Name</label>
-                        <input type="text" style="text-align:left; display: block; width: 300px" 
+                        <input type="text" style="text-align:left; display: block; width: 400px" 
                           value="${orderData.customer_name || ""}" disabled>
                     </div>
               <div class="editwholesaleorder-group" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
@@ -175,7 +175,7 @@ class EditWholesaleOrder {
                     </div>
               <div class="editwholesaleorder-group" style="display:flex; flex-direction: row; justify-content: center; align-items: center">
                         <label style="text-align:left; display: block; width: 50px">Invoice#</label>
-                        <input type="text" style="text-align:left; display: block; width: 50px" maxlength="10" 
+                        <input type="text" style="text-align:left; display: block; width: 100px" maxlength="10" 
                           value="${orderData.invoice_no || ""}">
                     </div>
                 </div>
