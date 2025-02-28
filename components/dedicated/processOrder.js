968189c6990c62bd03ab7505bcb6ca7aacc3db00
bohoaus <bohoaus2012@gmail.com>
2025-03-01 09:04:16 +1100

    Update processOrder.js

--- components/dedicated/processOrder.js
+++ components/dedicated/processOrder.js
@@ -76,7 +76,7 @@ class ProcessOrder {
             <form id="dispatchForm" style="display: none;">
               <div class="form-group">
                 <label for="invoiceNumber">Invoice Number*</label>
-                <input type="text" id="invoiceNumber" value="invoiceNumber" required style="width: 100px" maxlength="15">
+                <input type="text" id="invoiceNumber" value="${this.invoiceNumber || ""}" required style="width: 100px" maxlength="15">
               </div>
               <div class="form-group">
                 <label for="dispatchState">Dispatch State*</label>
