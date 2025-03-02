ff124213b5a2873c6c02d61b89f620bbba86fe06
bohoaus <bohoaus2012@gmail.com>
2025-03-02 16:13:03 +1100

    Update weeklySummary.js

--- components/dedicated/weeklySummary.js
+++ components/dedicated/weeklySummary.js
@@ -41,9 +41,9 @@ class WeeklySummary {
               <div class="date-filter">
                 <label>Select Week Period:</label>
                 <div class="date-inputs">
-                  <input type="date" id="weekStart" class="date-input">
+                  <input style="width:100px; color:blue" type="date" id="weekStart" class="date-input">
                   <span>to</span>
-                  <input type="date" id="weekEnd" class="date-input" disabled>
+                  <input style="width:100px; color:blue" type="date" id="weekEnd" class="date-input" disabled>
                 </div>
               </div>
             </div>
