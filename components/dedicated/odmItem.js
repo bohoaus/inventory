fc1267bf1d405e080d36efbfbcf48536ae31eeb9
bohoaus <bohoaus2012@gmail.com>
2025-03-07 18:23:30 +1100

    Update odmItem.js

--- components/dedicated/odmItem.js
+++ components/dedicated/odmItem.js
@@ -205,8 +205,10 @@ class OdmItem {
     const sfactories = [
       "SS+8620",
       "YJ+8620",
+      "K-DH+8620",
       "K-MX+8620",
       "K-RD+8620",
+      "K-YED+8620",
       "K-YP+8620",
       "K-Z",
     ];
