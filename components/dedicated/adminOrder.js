6cbbc313649ac50988f34eb9e3f5cfa24198d5bd
bohoaus <bohoaus2012@gmail.com>
2025-03-10 19:23:04 +1100

    Update adminOrder.js

--- components/dedicated/adminOrder.js
+++ components/dedicated/adminOrder.js
@@ -429,8 +429,8 @@ class AdminOrder {
                       year: "numeric",
                       month: "2-digit",
                       day: "2-digit",
-                      hour: "2-digit",
-                      minute: "2-digit",
+//                      hour: "2-digit",
+//                      minute: "2-digit",
                     }
                   );
                 } else {
@@ -515,8 +515,8 @@ class AdminOrder {
               year: "numeric",
               month: "2-digit",
               day: "2-digit",
-              hour: "2-digit",
-              minute: "2-digit",
+//              hour: "2-digit",
+//              minute: "2-digit",
             }
           );
         } else {
