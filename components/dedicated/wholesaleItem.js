e7a6a227da6e0ceb4c30c70876d7509ea82b43fa
bohoaus <bohoaus2012@gmail.com>
2025-03-16 09:48:05 +1100

    Update wholesaleItem.js

--- components/dedicated/wholesaleItem.js
+++ components/dedicated/wholesaleItem.js
@@ -494,7 +494,7 @@ class WholesaleItem {
 
             <div class="form-group-boho">
                 <label for="item_note" style="width: 150px">Item Note</label>
-                <textarea name="item_note" placeholder="OK" rows="5" style="width: 300px">${
+                <textarea name="item_note" placeholder="OK" rows="5" style="width: 250px">${
                   item?.item_note || "OK"
                 }</textarea>
             </div>
