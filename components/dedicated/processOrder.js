2df45ed1ae450dbbc3df71126f62e119d5887e0a
bohoaus <bohoaus2012@gmail.com>
2025-03-16 17:45:11 +1100

    Update processOrder.js

--- components/dedicated/processOrder.js
+++ components/dedicated/processOrder.js
@@ -80,17 +80,17 @@ class ProcessOrder {
             Please Dispatch Order Now:
             <form id="dispatchForm" style="display: block;">
               <div class="form-group-order-dispatch">
-                <label for="invoiceNumb">Invoice Number</label>
+                <label for="invoiceNumb" style="width: 120px">Invoice Number</label>
                 <input type="text" id="invoiceNumb" style="width: 80px" maxlength="15" required>
               </div>
               
             <div class="form-group-order-dispatch required">
-                <label for="dispatchDate8">Dispatch Date</label>
+                <label for="dispatchDate8" style="width: 120px">Dispatch Date</label>
                 <input type="date" id="dispatchDate8" style="width: 110px; color:blue" maxlength="25" required>
             </div>
               
               <div class="form-group-order-dispatch">
-                <label for="dispatchState">Dispatch State</label>
+                <label for="dispatchState" style="width: 120px">Dispatch State</label>
                 <select id="dispatchState" style="width: 150px; color:blue" required>
                   <option value="">Select State</option>
                   <option value="AUS-ACT">AUS-ACT</option>
@@ -106,11 +106,11 @@ class ProcessOrder {
                 </select>
               </div>
               <div class="form-group-order-dispatch">
-                <label for="dispatchBox">Dispatch Box</label>
+                <label for="dispatchBox" style="width: 120px">Dispatch Box</label>
                 <input type="text" id="dispatchBox" value="1" style="width: 50px; color:blue; maxlength:3" required>
               </div>
               <div class="form-group-order-dispatch">
-                <label for="dispatchCarrier">Dispatch Carrier</label>
+                <label for="dispatchCarrier" style="width: 120px">Dispatch Carrier</label>
                 <select id="dispatchCarrier" style="width: 200px; color:blue" required onchange="processOrder.toggleTrackingNumber()">
                   <option value="">Select Carrier</option>
                   <option value="DIRECT EXPRESS">DIRECT EXPRESS</option>
@@ -122,11 +122,11 @@ class ProcessOrder {
                 </select>
               </div>
               <div class="form-group-order-dispatch" id="trackingNumberGroup">
-                <label for="trackingNumber">Tracking Number</label>
+                <label for="trackingNumber" style="width: 120px">Tracking Number</label>
                 <input type="text" id="trackingNumber" style="width: 160px; color:blue; maxlength:45" required>
               </div>
               <div class="form-group-order-dispatch">
-                <label for="orderNotes">Order Notes</label>
+                <label for="orderNotes" style="width: 120px">Order Notes</label>
                 <textarea id="orderNotes" rows="3" value="OK" style="width: 200px; color:blue; maxlength:50"></textarea>
               </div>
               <div class="form-actions">
