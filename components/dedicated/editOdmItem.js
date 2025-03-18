4e5e184cdc0e789323f18245666e90a7d74ca54a
bohoaus <bohoaus2012@gmail.com>
2025-03-18 16:51:19 +1100

    Update editOdmItem.js

--- components/dedicated/editOdmItem.js
+++ components/dedicated/editOdmItem.js
@@ -264,7 +264,7 @@ class EditOdmItem {
         
         <div class="form-group-odm-edit">
           <label for="item_note" style="width: 200px">Note</label>
-          <textarea name="item_note" maxlength="50"  style="color: red;" 
+          <textarea name="item_note" maxlength="50" style="color: red;" 
                     rows="5"
                     onkeyup="this.value = this.value.toUpperCase()">${
                       item.item_note || ""
