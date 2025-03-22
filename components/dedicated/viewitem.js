ac138b0bc7b95b0e652aaf43220847c7e86170b9
bohoaus <bohoaus2012@gmail.com>
2025-03-22 20:48:12 +1100

    Update viewitem.js

--- components/dedicated/viewitem.js
+++ components/dedicated/viewitem.js
@@ -307,7 +307,7 @@ class ViewItem {
                 </div>
                 <div class="detail-row">
                     <span class="label">Arrive Date:</span>
-                    <span class="value">${new Date(item.arrive_date).toDateString() || null}</span>
+                    <span class="value" style="color:blue; font-size: 12px">${new Date(item.arrive_date).toDateString() || null}</span>
                 </div>
               </div>
             </div>
