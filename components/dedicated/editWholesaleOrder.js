b4d7c72a85e59f637f4b7d4af72075d735ba7ad4
bohoaus <bohoaus2012@gmail.com>
2025-04-14 21:06:23 +1000

    Update editWholesaleOrder.js

--- components/dedicated/editWholesaleOrder.js
+++ components/dedicated/editWholesaleOrder.js
@@ -117,7 +117,7 @@ class EditWholesaleOrder {
                 <button type="submit" class="editwholesaleorder-btn editwholesaleorder-btn-primary">Update Order</button>
               </div>
               <div class="editwholesaleorder-group" style="display:flex; flex-direction: row; justify-content: center; align-items: center; width: 300px">
-                        <label style="text-align:left; display: block; width: 400px"">Customer Name</label>
+                        <label style="text-align:left; display: block; width: 500px"">Customer Name</label>
                         <input type="text" style="text-align:left; display: flexi; width: 400px" 
                           value="${orderData.customer_name || ""}" disabled>
                     </div>
