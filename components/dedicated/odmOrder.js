edfea6ee23cc99e8207f86b8f1e4243e30e77b28
bohoaus <bohoaus2012@gmail.com>
2025-07-09 21:38:09 +1000

    Update odmOrder.js

--- components/dedicated/odmOrder.js
+++ components/dedicated/odmOrder.js
@@ -82,7 +82,7 @@ class OdmOrder {
                     </div>
                     <div class="form-group-odm-order">
                         <label for="invoice_no" style="color: grey">Invoice#</label>
-                        <input type="text" id="invoice_no" value="1217200" maxlength="10" style="width: 90px; color: blue">
+                        <input type="text" id="invoice_no" value="1217210" maxlength="10" style="width: 90px; color: blue">
                     </div>
 
                     <div class="form-group-odm-order required">
